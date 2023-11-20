53e0e6b8731785ab501f9bb9c4c1b6e02d0bbc0f
terence <terenceshawn18@outlook.com>
2023-11-20 15:23:15 +0300

    done

---
+++ src/components/GifList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function GifList({ gifs }) {
+  return (
+    <ul>
+      {gifs.map((gif) => (
+        <li key={gif.id}>
+          <img src={gif.images.fixed_height.url} alt={gif.title} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default GifList;
---
+++ src/components/GifSearch.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function GifSearch({ onSearchSubmit }) {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearchSubmit(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter a GIF search:
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </label>
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default GifSearch;
